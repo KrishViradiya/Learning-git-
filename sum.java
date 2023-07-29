6045af26134757c43390774ce0aca81e377b9299
Krish Viradiya <krishviradiya1303@gmail.com>
2023-07-29 14:50:41 +0530

    Added messaage in sum.java

--- sum.java
+++ sum.java
@@ -3,6 +3,6 @@ public class sum{
     public static void main(String[] args) {
         int a = 10;
         int b=3;
-        System.out.println(a+b);
+        System.out.println("The Sum is " +(a+b));
     }
 }
